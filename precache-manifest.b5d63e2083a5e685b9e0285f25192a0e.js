1491101fa92b87ffe2f7dfad00b1d6cf41c2f718
mehboobunnisa <kuroosa846@gmail.com>
2023-12-25 20:09:30 +0530

    Updates

---
+++ precache-manifest.b5d63e2083a5e685b9e0285f25192a0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b4485e449cca085c6acf8a064b26da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51d8df4b1af9e8e3e438",
+    "url": "/static/css/main.36da1666.chunk.css"
+  },
+  {
+    "revision": "e55c71b3c9005db11ebd",
+    "url": "/static/js/2.f29734d9.chunk.js"
+  },
+  {
+    "revision": "51d8df4b1af9e8e3e438",
+    "url": "/static/js/main.e8be8fa8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
